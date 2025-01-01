d12fbfdf01eebbaccfc820e6352019e4951e3d3a
chaylaz <chayla0504@gmail.com>
2025-01-02 06:24:49 +0700

    add feature story detail.

---
+++ lib/book_detail_page.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+class BookDetailPage extends StatelessWidget {
+  final String title;
+  final String image;
+  final String description;
+
+  const BookDetailPage({
+    Key? key,
+    required this.title,
+    required this.image,
+    required this.description,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green[200],
+        title: Text(
+          title,
+          style: TextStyle(
+            color: Colors.black87,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.black87),
+          onPressed: () => Navigator.pop(context),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: double.infinity,
+              height: 300,
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+              ),
+              child: Hero(
+                tag: 'book-$title',
+                child: Image.asset(
+                  image,
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black87,
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  Text(
+                    'Sinopsis',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black87,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    description,
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black87,
+                      height: 1.5,
+                    ),
+                    textAlign: TextAlign.justify,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/book_card.dart
+++ lib/widgets/book_card.dart
@@ -1,33 +1,89 @@
+// import 'package:flutter/material.dart';
+
+// class BookCard extends StatelessWidget {
+//   final String title;
+//   final String image;
+
+//   BookCard({required this.title, required this.image, required String description});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       width: 120,
+//       margin: EdgeInsets.only(right: 10),
+//       child: Column(
+//         crossAxisAlignment: CrossAxisAlignment.start,
+//         children: [
+//           Container(
+//             height: 150,
+//             decoration: BoxDecoration(
+//               color: Colors.grey[300],
+//               image: DecorationImage(
+//                 image: AssetImage(image),
+//                 fit: BoxFit.cover,
+//               ),
+//             ),
+//           ),
+//           SizedBox(height: 5),
+//           Text(title,
+//               style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold)),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+
+import 'package:chapteriaapp/screens/home_page.dart';
 import 'package:flutter/material.dart';
 
 class BookCard extends StatelessWidget {
   final String title;
   final String image;
+  final String description; // Tambahkan parameter description
 
-  BookCard({required this.title, required this.image, required String description});
+  BookCard({
+    required this.title,
+    required this.image,
+    required this.description, // Tambahkan parameter description
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 120,
-      margin: EdgeInsets.only(right: 10),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            height: 150,
-            decoration: BoxDecoration(
-              color: Colors.grey[300],
-              image: DecorationImage(
-                image: AssetImage(image),
-                fit: BoxFit.cover,
-              ),
+    return GestureDetector( // Tambahkan GestureDetector untuk menangani klik
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => BookDetailPage(
+              title: title,
+              image: image,
+              description: description,
             ),
           ),
-          SizedBox(height: 5),
-          Text(title,
-              style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold)),
-        ],
+        );
+      },
+      child: Container(
+        width: 120,
+        margin: EdgeInsets.only(right: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 150,
+              decoration: BoxDecoration(
+                color: Colors.grey[300],
+                image: DecorationImage(
+                  image: AssetImage(image),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            SizedBox(height: 5),
+            Text(title,
+                style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold)),
+          ],
+        ),
       ),
     );
   }
